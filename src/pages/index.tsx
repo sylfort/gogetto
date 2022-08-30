40ecd2f1a41f592ebcfbba022b4f07fc6ed9f2cf
Sylvio <sylviomafort@gmail.com>
2022-08-30 15:41:01 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Gogetto</h1>
+    </main>
+  );
+};
+
+export default Home;
